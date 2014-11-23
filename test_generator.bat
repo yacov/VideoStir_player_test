7d2b630e40e769a1029f54ca436fe7b50c7c7019
Iakov Volf <jakoff@gmail.com>
2014-11-23 09:17:38 +0200

    Changed input file name

--- test_generator.bat
+++ test_generator.bat
@@ -1,2 +1,2 @@
 @rem |----our perl script----|--input--file--|-widt-|-height-|						 |		
-perl perl\player_test_added.pl in\inputBig3.txt 1024 800
+perl perl\player_test_added.pl in\input.txt 1517 783
